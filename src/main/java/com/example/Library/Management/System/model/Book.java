16bb23b3ba8047f0e91b1ba50d1bab5f18506756
Pradip1807 <kumarpradip1807@gmail.com>
2024-10-01 17:52:57 +0530

    Initial Commit

--- src/main/java/com/example/Library/Management/System/model/Book.java
+++ src/main/java/com/example/Library/Management/System/model/Book.java
@@ -1,9 +1,6 @@
 package com.example.Library.Management.System.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Book {
